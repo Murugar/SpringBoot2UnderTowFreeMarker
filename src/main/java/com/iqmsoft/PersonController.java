e4e430d6da3973bbdcb5546836e13ffe64337a5f
Davanon <davanon2014@gmail.com>
2018-04-28 15:13:43 +0530

    First

---
+++ src/main/java/com/iqmsoft/PersonController.java
@@ -0,0 +1,25 @@
+package com.iqmsoft;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class PersonController {
+    @GetMapping("/")
+    public String index() {
+        return "redirect:/form";
+    }
+
+    @GetMapping("/form")
+    public String formGet() {
+        return "form";
+    }
+
+    @PostMapping("/form")
+    public String formPost(Person user, Model model) {
+        model.addAttribute("user", user);
+        return "form";
+    }
+}
